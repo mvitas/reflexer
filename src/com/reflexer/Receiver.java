c98cbd8bc2899420e135dda5efe07d0721583e19
Ivan Kušt <ivan@Bullet-Hole.local>
2013-01-29 20:19:53 +0100

    Added test BroadcastReceiver for the system intents

---
+++ src/com/reflexer/Receiver.java
@@ -0,0 +1,14 @@
+package com.reflexer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class Receiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d("Receiver", "intent: " + intent);
+	}
+}

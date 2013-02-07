442a8e41000965b89c30c66f9aca93ac3d7a3927
Ivan Kušt <ivan@Bullet-Hole.local>
2013-02-07 21:52:29 +0100

    Fixed permissions in android manifest. Added code for fetcing WiFi state Receiver

--- src/com/reflexer/Receiver.java
+++ src/com/reflexer/Receiver.java
@@ -3,6 +3,9 @@ package com.reflexer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 
 public class Receiver extends BroadcastReceiver {
@@ -10,5 +13,23 @@ public class Receiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Log.d("Receiver", "intent: " + intent);
+		
+		if(intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+			//int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN);
+			
+			NetworkInfo netInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+			
+			WifiManager wifiManager= (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+			WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+			
+			Log.d("Receiver", "network info: " + netInfo);
+			Log.d("Receiver", "wifi info: " + wifiInfo);
+		} else if(intent.getAction().equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)) {
+			boolean connected = intent.getBooleanExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, false);
+			
+			
+			
+			Log.d("Receiver", "network connected: " + connected);
+		}
 	}
 }

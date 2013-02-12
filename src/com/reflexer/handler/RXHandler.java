be5eab329f9a946df2d5ab5590d4b4bd614e0874
Marko V <marko.vitas@infinum.hr>
2013-02-12 23:48:53 +0100

    refactor packagea i imena klasa

---
+++ src/com/reflexer/handler/RXHandler.java
@@ -0,0 +1,5 @@
+package com.reflexer.handler;
+
+public abstract class RXHandler {
+
+}
---
+++ src/com/reflexer/handler/RXWifiHandler.java
@@ -0,0 +1,10 @@
+package com.reflexer.handler;
+
+public class RXWifiHandler extends RXHandler {
+
+	private boolean connected;
+	
+	private String currentNetwork;
+	
+	
+}

e8c6a2e820d59487228cf3927768126186f00725
Dario Penic <dario.penic@infinum.hr>
2013-02-28 01:18:41 +0100

    reaction fragment

--- src/com/reflexer/ui/RXReactionFragment.java
+++ src/com/reflexer/ui/RXReactionFragment.java
@@ -1,25 +1,45 @@
+
 package com.reflexer.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import antistatic.spinnerwheel.AbstractWheel;
+import antistatic.spinnerwheel.OnWheelChangedListener;
+
 import com.reflexer.R;
+import com.reflexer.ui.adapters.RXStimuliAdapter;
 
 public class RXReactionFragment extends Fragment {
 
-	public static RXReactionFragment newInstance() {
-		RXReactionFragment fragment = new RXReactionFragment();
+    public static RXReactionFragment newInstance() {
+        RXReactionFragment fragment = new RXReactionFragment();
 
-		return fragment;
-	}
+        return fragment;
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		return inflater.inflate(R.layout.activity_splash, null);
-	}
+        View view = inflater.inflate(R.layout.fragment_reaction, null);
+
+        AbstractWheel reactionWheel = (AbstractWheel)view.findViewById(R.id.reaction);
+
+        reactionWheel.setViewAdapter(new RXStimuliAdapter(getActivity()));
+        reactionWheel.setVisibleItems(7);
+        reactionWheel.addChangingListener(new OnWheelChangedListener() {
+
+            @Override
+            public void onChanged(AbstractWheel wheel, int oldValue, int newValue) {
+                Log.d("RXReactionFragment", "OnWheelChanged: " + newValue);
+            }
+        });
+
+        return view;
+    }
 
 }

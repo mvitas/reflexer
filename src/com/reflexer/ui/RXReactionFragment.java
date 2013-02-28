22b598f70001b7de68c4c37995d350a3a91ab42f
Dario Penic <dario.penic@infinum.hr>
2013-02-28 17:37:12 +0100

    reaction fragment

--- src/com/reflexer/ui/RXReactionFragment.java
+++ src/com/reflexer/ui/RXReactionFragment.java
@@ -7,9 +7,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import antistatic.spinnerwheel.AbstractWheel;
@@ -28,7 +31,8 @@ public class RXReactionFragment extends Fragment {
 
     ArrayList<String> list = new ArrayList<String>();
     RXReaction reaction;
-    
+    private ArrayList<RXReactionDefinition> reactions;
+    private LinearLayout conditionsLayout;
 
     public static RXReactionFragment newInstance() {
         RXReactionFragment fragment = new RXReactionFragment();
@@ -41,52 +45,58 @@ public class RXReactionFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_reaction, null);
 
-        // AbstractWheel reactionWheel =
-        // (AbstractWheel)view.findViewById(R.id.reaction);
-        //
-        // reactionWheel.setViewAdapter(new RXStimuliAdapter(getActivity()));
-        // reactionWheel.setVisibleItems(7);
-        // reactionWheel.addChangingListener(new OnWheelChangedListener() {
-        //
-        // @Override
-        // public void onChanged(AbstractWheel wheel, int oldValue, int
-        // newValue) {
-        // Log.d("RXReactionFragment", "OnWheelChanged: " + newValue);
-        // }
-        // });
+        getReactionDefinitions();
 
-        try {
-            ArrayList<RXReactionDefinition> reactions = RXReaction.getReactionDefinitions(getActivity());
-            for (RXReactionDefinition reaction : reactions) {
-                Log.d("DARIO", "reaction=" + reaction.getName());
-                list.add(reaction.getName());
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        setupReactionPicker(view);
+
+        conditionsLayout = (LinearLayout)view.findViewById(R.id.conditions_layout);
+
+        if (reaction == null) {
+            initReactionFromDefinitions(0);
         }
-
-        Spinner spinner = (Spinner)view.findViewById(R.id.reaction_spinner);
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-//                this.stimuli = new RXStimuli(RXStimuli.getStimuliDefinitions(getActivity()).get(index));
-//                RXReaction.get
-                
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-                
-            }
-        });
 
         return view;
     }
 
+    private void setupReactionPicker(View rootView) {
+        ImageView leftReactionPicker = (ImageView)rootView.findViewById(R.id.stimuli_picker_left);
+        ImageView rightReactionPicker = (ImageView)rootView.findViewById(R.id.stimuli_picker_right);
+
+        leftReactionPicker.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        rightReactionPicker.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void getReactionDefinitions() {
+
+        try {
+            reactions = RXReaction.getReactionDefinitions(getActivity());
+            for (RXReactionDefinition reaction : reactions) {
+                list.add(reaction.getName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initReactionFromDefinitions(int index) {
+        this.reaction = RXReaction.createReaction(getActivity(), reactions.get(index).getName());
+        showConditions();
+    }
+
+    private void showConditions() {
+
+    }
 }
